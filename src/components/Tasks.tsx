bae76c8e9c9db4636865025ca768ee91e9667cac
Gaaaybe <gabenesus@gmail.com>
2025-04-19 11:02:13 -0300

    feat: add task details page and integrate routing with react-router-dom

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,4 +1,5 @@
 import { ChevronRight, Trash } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 interface TaskProps {
   tasks:{
@@ -11,8 +12,22 @@ interface TaskProps {
   onTaskDelete: (taskId: number) => void;
 }
 
+interface Task {
+  id: number;
+  title: string;
+  description: string;
+  isCompleted: boolean;
+}
+
 
 function Tasks({ tasks, onTaskClick, onTaskDelete }: TaskProps) {
+  const navigate = useNavigate();
+  function onSeeDetailsClick(task: Task) {
+    const query = new URLSearchParams();
+    query.set("title", task.title);
+    query.set("description", task.description);
+    navigate(`/task?${query.toString()}`);
+  }
   return (
     <ul className="space-y-4 bg-slate-200 p-6 rounded-md shadow">
       {tasks.map((task) => (
@@ -22,7 +37,7 @@ function Tasks({ tasks, onTaskClick, onTaskDelete }: TaskProps) {
               className={`flex-1 p-2 rounded-md ${task.isCompleted ? "line-through" : ""} bg-slate-500 text-white`}>
               {task.title}
             </button>
-            <button className="bg-slate-400 p-2 rounded-md text-white">
+            <button onClick={()=>{onSeeDetailsClick(task)}} className="bg-slate-400 p-2 rounded-md text-white">
               <ChevronRight  />
             </button>
             <button onClick={() => onTaskDelete(task.id)}
